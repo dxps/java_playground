163f28caff1c916b3b81058b036d15ecbc464856
vision8 tech <vision8r@gmail.com>
2017-07-31 23:43:34 +0300

    AppStart beginnings

---
+++ standalone-embedded-jetty-rest/src/test/java/tech/vision8/embjetty/app/EmbJettyAppStart.java
@@ -0,0 +1,13 @@
+package tech.vision8.embjetty.app;
+
+/**
+ * @author vision8
+ */
+public class EmbJettyAppStart {
+
+    public static void main(String... args) {
+
+        // TODO: externalize Jetty specific startup into WebServer comp.
+    }
+
+}
